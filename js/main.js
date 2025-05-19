fe95fb7654359b7de4b8baff0d5a20691a726247
Alberto Noguchi <alberto2611@gmail.com>
2025-05-19 18:09:54 -0300

    add listing heroes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-// Start writing JavaScript here!
+import zlFetch from "https://cdn.jsdelivr.net/npm/zl-fetch@6.0.0/src/index.js";
+
+const dotaApi = "https://api.opendota.com/api";
+
+const heroesList = document.querySelector(".heroes-list");
+
+zlFetch(`${dotaApi}/constants/heroes`)
+  .then((response) => {
+    const heroes = Object.values(response.body);
+
+    heroes.forEach((hero) => {
+      const li = document.createElement("li");
+      li.classList.add("hero");
+      li.innerHTML = `
+          <a href="#">
+          <span class="hero__name">${hero.localized_name}</span>
+          <img src="https://api.opendota.com${hero.icon}" alt="${hero.localized_name}">
+        </a>
+      `;
+      heroesList.appendChild(li);
+    });
+  })
+  .catch((error) => console.log(error));
